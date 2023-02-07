e74fec3da8592751b4b3fc034f7081171bf7acf8
mohamdkarm <mohamdkarm551@gmail.com>
2023-02-07 20:18:23 +0200

    edit file bootstrap and  extracted files

--- templates/foot.php
+++ templates/foot.php
@@ -6,9 +6,7 @@ require_once(__ROOT__ . "/widgets/footer.php");
 </div>
 <script src="//<?= f__r("js/libraries/libs.js"); ?>"></script>
 <script src="//<?= f__r("js/script.js"); ?>"></script>
-<script src="//<?= f__r("js/libraries/1-jquery.js"); ?>"></script>
-<script src="//<?= f__r("js/bootstrap.min.js"); ?>"></script>
-<script src="//<?= f__r("js/jquery.min.js"); ?>"></script>
+
 </body>
 
 </html>
