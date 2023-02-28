24696042b125469ac1ed7e6a5cee4d98d586eca9
mohamdkarm <mohamdkarm551@gmail.com>
2023-02-28 23:56:55 +0200

    added videos page

---
+++ assets/js/video-list.js
@@ -0,0 +1,67 @@
+let allVideos = [
+  {
+    name: "Computer Monitor Shape",
+    src: "new",
+    id: "vid_1",
+  },
+  {
+    name: "Button Hover Effect",
+    src: "HAARPER - PUKE",
+    id: "vid_2",
+  },
+  {
+    name: "Confirm Password using Html CSS & js",
+    src: "Confirm Password using Html CSS & js",
+    id: "vid_3",
+  },
+  {
+    name: "Creative Card Hover Effect",
+    src: "Creative Card Hover Effect ",
+    id: "vid_4",
+  },
+  {
+    name: "Creative Digital Clock Ui Design",
+    src: "Creative Digital Clock Ui Design - Html Css & Vanilla Javascript-1",
+    id: "vid_5",
+  },
+  {
+    name: "Creative Our Team Section",
+    src: "Creative Our Team Section Using HTML & CSS Neumorphism",
+    id: "vid_6",
+  },
+  {
+    name: "Filter Text Animation Using Html and CSS",
+    src: "Filter Text Animation Using Htm and Pure CSS",
+    id: "vid_7",
+  },
+  {
+    name: "Glassmorphism Calculater New Design",
+    src: "Glassmorphism Calculater New Design Html",
+    id: "vid_8",
+  },
+  {
+    name: "How To  Make Cheatsheet",
+    src: "How To  Make Cheatsheet Using Html, CSS and Javascript",
+    id: "vid_9",
+  },
+  {
+    name: "How to create Login Form",
+    src: "How to create Login Form Using Html and CSS",
+    id: "vid_10",
+  },
+  {
+    name: "How To Make Animated Menu Icon",
+    src: "How To Make Animated Menu Icon Using Html",
+    id: "vid_11",
+  },
+  {
+    name: "How to make Read More Function",
+    src: "How to make Read More Function using Html",
+    id: "vid_12",
+  },
+  {
+    name: "Vertical Navigation Bar",
+    src: "Vertical Navigation Bar Using Html And CSS",
+    id: "vid_13",
+  },
+];
--- templates/foot.php
+++ templates/foot.php
@@ -7,6 +7,8 @@ require_once(__ROOT__ . "/widgets/footer.php");
 </div>
 <script src="//<?= f__r("js/libraries/libs.js"); ?>"></script>
 <script src="//<?= f__r("js/script.js"); ?>"></script>
+<script src="//<?= f__r("js/videos.js"); ?>"></script>
+<script src="//<?= f__r("js/video-list.js"); ?>"></script>
 
 </body>
 
