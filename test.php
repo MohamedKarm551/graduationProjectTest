46bfcbedf5b64c5d6dd594c7de06d4979aa6216b
MohamedKarm551 <mohamdkarm551@gmail.com>
2023-06-23 20:46:05 +0300

    23-6-2023 8:45  karam edit footer , images slider , style path page

--- test.php
+++ test.php
@@ -1,25 +1,4 @@
 <?php
-$the_hml_data = '<h2>What is Web Design?</h2>
-    <p>Web design is one of the most essential parts to build a complete website. Web design is what creates the overall look of a website and It’s the process of planning and building the elements of your website, from structure and layout to images, colours, fonts and graphics.
-
-        A web designer works on the website layout, the design that we can see. Web Design and Web Development are not the same. But without Web Design, Web development can’t be done. So before Web Development, you need to understand web design and its roadmap.</p>
-    <h2>Complete Web Design Roadmap for beginners</h2>
-    <p>Pretty much sure that whenever you browse a website, the first thing you see is the look and feel of it. Undoubtedly, no one prefers the website or application having an inferior user interface or design.</p>
-    <p>And the man who ensures this better look and feel & user interface for the websites is Front-End Developer!! If you want to be a successful Front-end-developer then you need to understand the core things of Web Design.</p>
-    <ul>
-        <li>HTML : HTML stands for HyperText Markup Language. It is used to design the front end of web pages. It acts as a skeleton for a website since it is used to make the structure of a website.</li>
-        <li>CSS : Cascading Style Sheets or CSS is simply a designed language intended to simplify the process of making web pages presentable. It is used to style our website. There are some Libraries, Frameworks and Preprocessors available which helps to design more effectivaly and add more features on website.</li>
-        <li>Pure CSS : Pure CSS or Core CSS is the main elements of Cascading Style Sheets. After HTML you need to learn the Pure CSS elements.</li>
-        <li>Bootstrap : Bootstrap is one of the most popular and open-source CSS framework for creating responsive websites and web applications. It is the mostly used tool collection for developing responsive, mobile-first websites.</li>
-        <li>SASS : Sass is a stylesheet language and CSS preprocessor that’s compiled to CSS. It allows you to use variables, nested rules, mixins, functions, and more, all with a fully CSS-compatible syntax. It helps keep large stylesheets well-organized and makes it easy to share design within and across projects.</li>
-        <li>JavaScript Frameworks and Libraries :</li>
-        <li>Angular JS</li>
-        <li>React JS</li>
-        <li>Vue JS</li>
-        <li>JQuery</li>
-    </ul>
-    <h2>Where to learn?</h2>
-    <p>The best way to learn HTML, CSS and JavaScript is by following the official documents of these languages.&nbsp;<a href="https://www.w3schools.com/" target="_blank" rel="nofollow noopener" waprocessedanchor="true">W3Schools</a>&nbsp;is a free and best platform for beginners to learn Web Design completely.</p>
-    <p>There are several ways to take a course and learn web design.</p>';
+$the_hml_data = '';
 $html_code = htmlentities(htmlspecialchars($the_hml_data));
 echo $html_code;
