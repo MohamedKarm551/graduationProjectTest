2c8d65bc4e02bd3a42a8ca278254d5f495ce4456
abdelrahman-gamal <abdelrahmangamal219@gmail.com>
2023-07-05 15:53:58 +0300

    lag fixed

--- templates/foot.php
+++ templates/foot.php
@@ -7,12 +7,6 @@ require_once(__ROOT__ . "/widgets/footer.php");
 </div>
 <script src="//<?= f__r("js/libraries/libs.js"); ?>"></script>
 <script src="//<?= f__r("js/script.js"); ?>"></script>
-<script src="//<?= f__r("js/videos.js"); ?>"></script>
-<script src="//<?= f__r("js/video-list.js"); ?>"></script>
-<script src="//<?= f__r("js/accord2.js"); ?>"></script>
-<script src="//<?= f__r("js/MaterialDescription.js"); ?>"></script>
-<script src="//<?= f__r("js/questions.js"); ?>"></script>
-
 </body>
 
 </html>
