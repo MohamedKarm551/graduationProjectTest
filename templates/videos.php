dcdfd66ebddecb787fa7906f55aca67037c002b0
abdelrahman-gamal <abdelrahmangamal219@gmail.com>
2023-07-06 19:22:10 +0300

    videos bug fixed

--- templates/videos.php
+++ templates/videos.php
@@ -2,9 +2,11 @@
 <section class="videos">
     <h2 class="title"></h2>
     <div class="container">
-        <div id="video_player">
-            <video controls id="main-Video" src=""></video>
-        </div>
+        <iframe
+                id="main-Video"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen>
+        </iframe>
         <div class="playlistBx">
             <div class="header">
                 <div class="row">
