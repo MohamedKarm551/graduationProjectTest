f3fadd7b31c813eebfb2e6457cde92551b229b87
MohamedKarm551 <mohamdkarm551@gmail.com>
2023-07-10 04:29:30 +0300

    karam edit in videos  10-7-2023

--- assets/js/video-list.js
+++ assets/js/video-list.js
@@ -25,49 +25,10 @@ let allVideos = [
     src: "https://www.youtube.com/embed/aCp0CJQqlj4",
     id: "vid_5",
   },
-  // {
-  //   name: "Creative Digital Clock Ui Design",
-  //   src: "Creative Digital Clock Ui Design - Html Css & Vanilla Javascript-1",
-  //   id: "vid_5",
-  // },
-  // {
-  //   name: "Creative Our Team Section",
-  //   src: "Creative Our Team Section Using HTML & CSS Neumorphism",
-  //   id: "vid_6",
-  // },
-  // {
-  //   name: "Filter Text Animation Using Html and CSS",
-  //   src: "Filter Text Animation Using Htm and Pure CSS",
-  //   id: "vid_7",
-  // },
-  // {
-  //   name: "Glassmorphism Calculater New Design",
-  //   src: "Glassmorphism Calculater New Design Html",
-  //   id: "vid_8",
-  // },
-  // {
-  //   name: "How To  Make Cheatsheet",
-  //   src: "How To  Make Cheatsheet Using Html, CSS and Javascript",
-  //   id: "vid_9",
-  // },
-  // {
-  //   name: "How to create Login Form",
-  //   src: "How to create Login Form Using Html and CSS",
-  //   id: "vid_10",
-  // },
-  // {
-  //   name: "How To Make Animated Menu Icon",
-  //   src: "How To Make Animated Menu Icon Using Html",
-  //   id: "vid_11",
-  // },
-  // {
-  //   name: "How to make Read More Function",
-  //   src: "How to make Read More Function using Html",
-  //   id: "vid_12",
-  // },
-  // {
-  //   name: "Vertical Navigation Bar",
-  //   src: "Vertical Navigation Bar Using Html And CSS",
-  //   id: "vid_13",
-  // },
+  {
+    name: "Intro02",
+    src: "https://www.youtube.com/embed/WF6ZmYsB1xs",
+    id: "vid_6",
+  },
+
 ];
